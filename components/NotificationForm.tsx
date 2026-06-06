a2c0c1a963af00fca2c6a99e7b49d6fe826c08ea
tonguiino <santiagotq2@gmail.com>
2026-06-06 01:28:07 -0500

    feat: pass setNotifications prop to NotificationForm and implement form state management

--- components/NotificationForm.tsx
+++ components/NotificationForm.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import { NotificationJob } from '@/types/notification'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 
-const NotificationForm = () => {
+type SetNotification = {
+    setNotifications: Dispatch<SetStateAction<NotificationJob[]>>
+}
+const NotificationForm = ({ setNotifications, }: SetNotification) => {
+    const [title, setTitle] = useState('')
+    const [channel, setChannel] = useState('email')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+    }
     return (
         <>
             <div className="p-2 border-gray-300 border rounded-md shadow-xl">
-                <form action="" className='flex items-end gap-4'>
+                <form onSubmit={handleSubmit} className='flex items-end gap-4'>
                     <div className='flex flex-col flex-1'>
-                        <label htmlFor="">Notification Title</label>
-                        <input className='border rounded-md p-1' type="text" placeholder='Ej: Promo de Verano' />
+                        <label htmlFor="title">Notification Title</label>
+                        <input
+                            className='border rounded-md p-1'
+                            type="text"
+                            placeholder='Ej: Promo de Verano'
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+
+                        />
                     </div>
                     <div className='flex flex-col'>
-                        <label htmlFor="">Channel</label>
-                        <select className="border rounded-md p-1 w-48">
+                        <label htmlFor="channel">Channel</label>
+                        <select
+                            className="border rounded-md p-1 w-48"
+                            value={channel}
+                            onChange={e => setChannel(e.target.value)}
+                        >
                             <option value="email">Email</option>
                             <option value="sms">Sms</option>
                             <option value="push">Push</option>
